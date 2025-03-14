6bf945d9c3f43c33601692b2f2508f6c90fc3ab1
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-13 19:41:39 -0700

    update

--- install.js
+++ install.js
@@ -9,18 +9,6 @@ module.exports = {
         ]
       }
     },
-    // Delete this step if your project does not use torch
-    {
-      method: "script.start",
-      params: {
-        uri: "torch.js",
-        params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-        }
-      }
-    },
     // Edit this step with your custom install commands
     {
       method: "shell.run",
@@ -32,6 +20,18 @@ module.exports = {
           "uv pip install -r requirements.txt",
           "{{platform === 'darwin' ? 'uv pip install moshi-mlx==0.2.2' : 'pip install moshi==0.2.2'}}"
         ]
+      }
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+        }
       }
     },
 //    {
