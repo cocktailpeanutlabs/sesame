7c27bc6b734dbbf93629e477595d7d14162a7342
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-13 18:57:57 -0700

    update

--- install.js
+++ install.js
@@ -29,7 +29,8 @@ module.exports = {
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
           "uv pip install gradio devicetorch",
-          "uv pip install -r requirements.txt"
+          "uv pip install -r requirements.txt",
+          "{{platform === 'darwin' ? 'uv pip install moshi-mlx' : 'pip install moshi'}}"
         ]
       }
     },
